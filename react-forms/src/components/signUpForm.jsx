c9309eef77e99a1a23dd9910a2f25c39fcdc8a16
Pedro Perez <pipr96@gmail.com>
2023-07-27 19:34:18 -0700

    added components

---
+++ react-forms/src/components/signUpForm.jsx
@@ -0,0 +1,18 @@
+
+
+export default function SignUpForm(){
+    //handlers
+
+
+    //jsx
+    return(
+        <form action="">
+            <h3>Sign up Form</h3>
+            <label> Username:
+                <input type="text" />
+            </label>
+
+        </form>
+    )
+
+}
