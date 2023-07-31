6d3600eb1176ebb543a87d37dd77e013617e9f7a
Pedro Perez <pipr96@gmail.com>
2023-07-31 10:51:44 -0700

    add authentication process

--- react-forms/src/components/authenticate.jsx
+++ react-forms/src/components/authenticate.jsx
@@ -1,6 +1,38 @@
-export default function Authenticate(){
+import { useState } from "react";
+
+
+export default function Authenticate({token}){
+    const [successMessage, setSuccessMessage] = useState(null);
+    const [error, setError] = useState(null);
+
+    async function handler(){
+        try {
+            console.log("were in");
+            const response = await fetch('https://fsa-jwt-practice.herokuapp.com/authenticate', 
+              { 
+                method: "GET", 
+                headers: { 
+                  "Content-Type": "application/json",
+                  "Authorization": `Bearer ${token}` 
+                }
+              })
+              const result = await response.json();
+              setSuccessMessage(result.message);
+              console.log(result);
+        } catch (error) {
+            setError(error);
+        }
+
+
+    }
 
     return(
-        <h2>Authenticate</h2>
+        <>
+            <h3>Authenticate</h3>
+            {successMessage && <p>{successMessage}</p>}
+            {error && <p>{error}</p>}
+            <button onClick={handler}>Verify</button>
+            
+        </>
     )
 }
