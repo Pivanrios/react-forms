344cbe5fd045a86b2ef2c17f17ec365807f4deb8
Pedro Perez <pipr96@gmail.com>
2023-07-30 22:42:50 -0700

    add event handler

--- react-forms/src/components/signUpForm.jsx
+++ react-forms/src/components/signUpForm.jsx
@@ -6,16 +6,37 @@ export default function SignUpForm(){
     const [password, setPassword] = useState("");
     const [error, setError] = useState(null);
 
+
+    //submit button event handler
+    async function handleSubmit(e){
+        e.preventDefault(); //reset/clean function
+        console.log("inputs: ", userName, password);
+        try {
+            const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup");
+            const result = await response.json();
+            console.log(result);
+        } catch (error) {
+            console.log(error)
+            setError(error);
+        }
+    }
     //jsx
     return(
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
             <h3>Sign up Form</h3>
             <label> Username:{""}
-                <input type="text" />
+                <input 
+                type="text" 
+                value={userName} 
+                onChange={e=> setUserName(e.target.value)} />
             </label>
             <label> Password:
-                <input type="text" />
+                <input 
+                type="text"
+                value={password}
+                onChange={e=> setPassword(e.target.value)} />
             </label>
+            <p>{password}</p>
             <button>Submit</button>
         </form>
     )
